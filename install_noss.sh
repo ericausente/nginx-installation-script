b5cef69780718949dd7b3c6264ea15eebb5d719e
Jay Desai <52437445+jay-nginx@users.noreply.github.com>
2020-08-03 13:45:21 +1000

    fixed the /n
    fixed the /n

--- install_noss.sh
+++ install_noss.sh
@@ -2,8 +2,8 @@
 sudo apt-get update -y
 sudo wget https://nginx.org/keys/nginx_signing.key
 sudo apt-key add nginx_signing.key
-echo "deb https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx\n" | sudo tee -a /etc/apt/sources.list
-echo "deb-src https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx\n" | sudo tee -a /etc/apt/sources.list
+echo "deb https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" | sudo tee -a /etc/apt/sources.list
+echo "deb-src https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sleep 10
