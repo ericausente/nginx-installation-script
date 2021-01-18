0033909c416abc54d586181f41899537f1161bc5
Jay Desai <52437445+jay-nginx@users.noreply.github.com>
2021-01-18 22:57:22 +1100

    Update install_nplus.sh

--- install_nplus.sh
+++ install_nplus.sh
@@ -11,7 +11,7 @@ sudo cp /tmp/nginx-repo.key /etc/ssl/nginx/
 sudo wget https://nginx.org/keys/nginx_signing.key
 sudo apt-key add nginx_signing.key
 sudo apt-get update
-apt-get install apt-transport-https lsb-release ca-certificates -y
+sudo apt-get install apt-transport-https lsb-release ca-certificates -y -qq
 printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-plus.list
 sudo wget -q -O /etc/apt/apt.conf.d/90nginx https://cs.nginx.com/static/files/90nginx
 sudo apt-get update && sudo apt-get install -y nginx-plus
